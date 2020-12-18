fe0b66bebb213dfa34184c02e61ee840ab6eace6
Daniel Gerlinghoff <daniel_gerlinghoff@ihpc.a-star.edu.sg>
2020-12-18 19:29:42 +0800

    Add loop for multiple epochs Change result format and print into log file Add SGD learning for reference

--- desire_backprop.py
+++ desire_backprop.py
@@ -1,5 +1,6 @@
+import os
 import numpy as np
-from torchvision import datasets
+from torchvision import datasets, transforms
 import itertools
 
 # Network configuration
@@ -7,9 +8,11 @@ neurons   = (784, 512, 256, 10)
 layer_cnt = len(neurons) - 1
 tstep_cnt = 20
 image_cnt = 2000
+epoch_cnt = 1
 
 train_ntest   = True
 debug         = True
+debug_period  = 2000
 mempot_thres  = 1
 learning_rate = 1.e-3 / tstep_cnt
 decay         = 1
@@ -21,7 +24,6 @@ mempot  = [np.empty(0)] * (layer_cnt + 1)
 spikes  = [np.empty(0)] * (layer_cnt + 1)
 traces  = [np.empty(0)] * layer_cnt
 desire  = [np.empty(0)] * layer_cnt
-results = np.zeros((neurons[-1], neurons[-1]), dtype=np.int)
 
 np.random.seed(0)
 for layer in range(layer_cnt):
@@ -32,72 +34,15 @@ for layer in range(layer_cnt):
         weights[layer] = np.load(f"model/weights_{layer}.npy")
 
 # Iterate over MNIST images
-dataset = datasets.MNIST("data/", download=True, train=train_ntest)
-for (image_idx, (image, label)) in enumerate(itertools.islice(dataset, image_cnt)):
-    if debug: print(f"Image {image_idx}: {label}")
+dataset = datasets.MNIST("data", download=True, train=train_ntest, transform=transforms.ToTensor())
+logfile = open(os.path.splitext(os.path.basename(__file__))[0] + ("_train" if train_ntest else "_test") + ".log", "w")
+for epoch in range(epoch_cnt):
+    if debug: print(f"Epoch: {epoch}")
 
-    # Reset spikes and membrane potentials
-    for layer in range(layer_cnt + 1):
-        mempot[layer] = np.zeros(neurons[layer], dtype=np.float)
-        spikes[layer] = np.zeros((tstep_cnt + 1, neurons[layer]), dtype=np.bool)
-        if layer < layer_cnt:
-            traces[layer] = np.zeros((tstep_cnt + 1, neurons[layer]), dtype=np.float)
-            desire[layer] = np.zeros((neurons[layer+1], 2), dtype=np.bool)
-    
-    # Input image and forward pass
-    image = np.array(image).flatten().astype(np.float) / 255.
-
-    for tstep in range(tstep_cnt):
-        # Generate input spike train from image
-        for neu_in in range(neurons[0]):
-            mempot[0][neu_in] += image[neu_in]
-            if mempot[0][neu_in] >= mempot_thres:
-                spikes[0][tstep][neu_in] = True
-                mempot[0][neu_in] -= mempot_thres
-
-        # Propagate spikes forward
-        for layer in range(layer_cnt):
-            for neu_post in range(neurons[layer+1]):
-                # Update membrane potential
-                for neu_pre in range(neurons[layer]):
-                    if spikes[layer][tstep][neu_pre]:
-                        mempot[layer+1][neu_post] += weights[layer][neu_pre][neu_post]
-
-                # Calculate output spikes and decay membrane potential
-                if mempot[layer+1][neu_post] >= mempot_thres:
-                    spikes[layer+1][tstep+1][neu_post] = True
-                    mempot[layer+1][neu_post] -= mempot_thres
-                else:
-                    mempot_old = mempot[layer+1][neu_post]
-                    mempot[layer+1][neu_post] = ((mempot_old * 2 ** decay) - mempot_old) / 2 ** decay
-
-    # Training portion
-    if train_ntest:
-        # Backpropagate desire
-        for layer in [layer_cnt]:
-            # Output layer
-            for neu_pre in range(neurons[layer]):
-                if neu_pre == label:
-                    desire[layer-1][neu_pre][0] = (1 - np.sum(spikes[layer][:,neu_pre]) / tstep_cnt) >= desire_thres["output"]
-                    desire[layer-1][neu_pre][1] = True
-                else:
-                    desire[layer-1][neu_pre][0] = (np.sum(spikes[layer][:,neu_pre]) / tstep_cnt) >= desire_thres["output"]
-                    desire[layer-1][neu_pre][1] = False
-
-        for layer in range(layer_cnt - 1, 0, -1):
-            # Hidden layers
-            for neu_pre in range(neurons[layer]):
-                desire_sum = 0
-                for neu_post in range(neurons[layer+1]):
-                    if desire[layer][neu_post][0]:
-                        if desire[layer][neu_post][1]:
-                            target_mult = 1 - np.sum(spikes[layer+1][:,neu_post]) / tstep_cnt
-                            desire_sum += weights[layer][neu_pre][neu_post] * target_mult
-                        else:
-                            target_mult = np.sum(spikes[layer+1][:,neu_post]) / tstep_cnt
-                            desire_sum -= weights[layer][neu_pre][neu_post] * target_mult
-                desire[layer-1][neu_pre][0] = np.abs(desire_sum) >= desire_thres["hidden"]
-                desire[layer-1][neu_pre][1] = desire_sum > 0
+    for (image_idx, (image, label)) in enumerate(itertools.islice(dataset, image_cnt)):
+        if debug: print(f"Image {image_idx}: {label}")
+        if image_idx % debug_period == 0:
+            results = np.zeros((neurons[-1], neurons[-1]), dtype=np.int)
 
         # Reset spikes and membrane potentials
         for layer in range(layer_cnt + 1):
@@ -105,8 +50,11 @@ for (image_idx, (image, label)) in enumerate(itertools.islice(dataset, image_cnt
             spikes[layer] = np.zeros((tstep_cnt + 1, neurons[layer]), dtype=np.bool)
             if layer < layer_cnt:
                 traces[layer] = np.zeros((tstep_cnt + 1, neurons[layer]), dtype=np.float)
-        
-        # Process spikes and learn
+                desire[layer] = np.zeros((neurons[layer+1], 2), dtype=np.bool)
+
+        # Input image and forward pass
+        image = np.array(image, dtype=np.float).flatten()
+
         for tstep in range(tstep_cnt):
             # Generate input spike train from image
             for neu_in in range(neurons[0]):
@@ -131,28 +79,10 @@ for (image_idx, (image, label)) in enumerate(itertools.islice(dataset, image_cnt
                         mempot_old = mempot[layer+1][neu_post]
                         mempot[layer+1][neu_post] = ((mempot_old * 2 ** decay) - mempot_old) / 2 ** decay
 
-                # Update spike traces
-                for neu_pre in range(neurons[layer]):
-                    trace = traces[layer][tstep][neu_pre]
-                    traces[layer][tstep+1][neu_pre] = ((trace * 2 ** decay) - trace) / 2 ** decay
+        if debug:
+            print(np.sum(spikes[-1], axis=0))
 
-                    if spikes[layer][tstep][neu_pre]:
-                        traces[layer][tstep+1][neu_pre] += 1
-
-                # Update weights
-                if train_ntest:
-                    for neu_post in range(neurons[layer+1]):
-                        if spikes[layer+1][tstep+1][neu_post]:
-                            for neu_pre in range(neurons[layer]):
-                                if desire[layer][neu_post][0]:
-                                    update = learning_rate * traces[layer][tstep+1][neu_pre]
-                                    weights[layer][neu_pre][neu_post] += (1 if desire[layer][neu_post][1] else -1) * update
-
-    if debug:
-        print(np.sum(spikes[-1], axis=0))
-
-    # Compare output with label
-    if not train_ntest:
+        # Compare output with label
         spikes_out = np.sum(spikes[-1], axis=0)
         spikes_max = np.max(spikes_out)
         if spikes_out[label] == spikes_max:
@@ -160,12 +90,95 @@ for (image_idx, (image, label)) in enumerate(itertools.islice(dataset, image_cnt
         else:
             results[label][np.argmax(spikes_out)] += 1
 
-# Save network model and report accuracy
-if train_ntest:
-    for layer in range(layer_cnt):
-        np.save(f"model/weights_{layer}.npy", weights[layer])
-else:
-    print(results)
-    print("Accuracy: {:.2f}%".format(sum([results[idx, idx] for idx in range(neurons[-1])]) / image_cnt * 100))
+        if image_idx % debug_period == debug_period - 1:
+            print(f"Epoch: {epoch}, Image: {image_idx}", file=logfile)
+            print(results, file=logfile)
+            print("Accuracy: {:.2f}%".format(sum([results[idx, idx] for idx in range(neurons[-1])]) / debug_period * 100), file=logfile)
+            logfile.flush()
 
-pass
+        # Training portion
+        if train_ntest:
+            # Backpropagate desire
+            for layer in [layer_cnt]:
+                # Output layer
+                for neu_pre in range(neurons[layer]):
+                    if neu_pre == label:
+                        desire[layer-1][neu_pre][0] = (1 - np.sum(spikes[layer][:,neu_pre]) / tstep_cnt) >= desire_thres["output"]
+                        desire[layer-1][neu_pre][1] = True
+                    else:
+                        desire[layer-1][neu_pre][0] = (np.sum(spikes[layer][:,neu_pre]) / tstep_cnt) >= desire_thres["output"]
+                        desire[layer-1][neu_pre][1] = False
+
+            for layer in range(layer_cnt - 1, 0, -1):
+                # Hidden layers
+                for neu_pre in range(neurons[layer]):
+                    desire_sum = 0
+                    for neu_post in range(neurons[layer+1]):
+                        if desire[layer][neu_post][0]:
+                            if desire[layer][neu_post][1]:
+                                target_mult = 1 - np.sum(spikes[layer+1][:,neu_post]) / tstep_cnt
+                                desire_sum += weights[layer][neu_pre][neu_post] * target_mult
+                            else:
+                                target_mult = np.sum(spikes[layer+1][:,neu_post]) / tstep_cnt
+                                desire_sum -= weights[layer][neu_pre][neu_post] * target_mult
+                    desire[layer-1][neu_pre][0] = np.abs(desire_sum) >= desire_thres["hidden"]
+                    desire[layer-1][neu_pre][1] = desire_sum > 0
+
+            # Reset spikes and membrane potentials
+            for layer in range(layer_cnt + 1):
+                mempot[layer] = np.zeros(neurons[layer], dtype=np.float)
+                spikes[layer] = np.zeros((tstep_cnt + 1, neurons[layer]), dtype=np.bool)
+                if layer < layer_cnt:
+                    traces[layer] = np.zeros((tstep_cnt + 1, neurons[layer]), dtype=np.float)
+
+            # Process spikes and learn
+            for tstep in range(tstep_cnt):
+                # Generate input spike train from image
+                for neu_in in range(neurons[0]):
+                    mempot[0][neu_in] += image[neu_in]
+                    if mempot[0][neu_in] >= mempot_thres:
+                        spikes[0][tstep][neu_in] = True
+                        mempot[0][neu_in] -= mempot_thres
+
+                # Propagate spikes forward
+                for layer in range(layer_cnt):
+                    for neu_post in range(neurons[layer+1]):
+                        # Update membrane potential
+                        for neu_pre in range(neurons[layer]):
+                            if spikes[layer][tstep][neu_pre]:
+                                mempot[layer+1][neu_post] += weights[layer][neu_pre][neu_post]
+
+                        # Calculate output spikes and decay membrane potential
+                        if mempot[layer+1][neu_post] >= mempot_thres:
+                            spikes[layer+1][tstep+1][neu_post] = True
+                            mempot[layer+1][neu_post] -= mempot_thres
+                        else:
+                            mempot_old = mempot[layer+1][neu_post]
+                            mempot[layer+1][neu_post] = ((mempot_old * 2 ** decay) - mempot_old) / 2 ** decay
+
+                    # Update spike traces
+                    for neu_pre in range(neurons[layer]):
+                        trace = traces[layer][tstep][neu_pre]
+                        traces[layer][tstep+1][neu_pre] = ((trace * 2 ** decay) - trace) / 2 ** decay
+
+                        if spikes[layer][tstep][neu_pre]:
+                            traces[layer][tstep+1][neu_pre] += 1
+
+                    # Update weights
+                    if train_ntest:
+                        for neu_post in range(neurons[layer+1]):
+                            if spikes[layer+1][tstep+1][neu_post]:
+                                for neu_pre in range(neurons[layer]):
+                                    if desire[layer][neu_post][0]:
+                                        update = learning_rate * traces[layer][tstep+1][neu_pre]
+                                        weights[layer][neu_pre][neu_post] += (1 if desire[layer][neu_post][1] else -1) * update
+
+    # Save network model
+    if train_ntest:
+        if not os.path.exists(f"model_{epoch}"):
+            os.makedirs(f"model_{epoch}")
+        for layer in range(layer_cnt):
+            np.save(f"model_{epoch}/weights_{layer}.npy", weights[layer])
+
+logfile.close()
+
---
+++ sgd_reference.py
@@ -0,0 +1,72 @@
+import torch
+from torchvision import datasets, transforms
+
+# Define neural network model
+log_interval = 200
+torch.manual_seed(0)
+
+neurons = [784, 512, 256, 10]
+model = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(neurons[0], neurons[1], bias=False),
+    torch.nn.ReLU(),
+    torch.nn.Linear(neurons[1], neurons[2], bias=False),
+    torch.nn.ReLU(),
+    torch.nn.Linear(neurons[2], neurons[3], bias=False),
+    torch.nn.Softmax(dim=1)
+)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.05)
+criterion = torch.nn.CrossEntropyLoss()
+
+# Download and prepare MNIST dataset
+image_cnt  = 2000
+batch_size = 1
+batch_cnt  = int(image_cnt / batch_size)
+
+train_dataset = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
+train_loader  = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True) 
+test_dataset  = datasets.MNIST('data', train=False, transform=transforms.ToTensor()) 
+test_loader   = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+epoch_cnt = 1
+for epoch in range(epoch_cnt):
+    # Train network
+    model.train()
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if batch_idx >= batch_cnt: break
+
+        # Zero gradient buffers
+        optimizer.zero_grad()
+
+        # Pass data through the network
+        output = model(data)
+
+        # Calculate loss
+        loss = criterion(output, target)
+
+        # Backpropagate
+        loss.backward()
+
+        # Update weights
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), image_cnt,
+                100. * batch_idx / batch_cnt, loss.data.item()))
+
+    # Test network
+    model.eval()
+
+    correct = 0
+    for data_idx, (data, target) in enumerate(test_loader):
+
+        output  = model(data)
+        pred    = output.data.max(1)[1]
+        correct += pred.eq(target.data).cpu().sum()
+
+    accuracy = 100. * correct.to(torch.float32) / len(test_loader.dataset)
+
+    print('Test set: Accuracy: {}/{} ({:.0f}%)'.format(
+        correct, len(test_loader.dataset), accuracy))
